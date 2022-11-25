12d68fb95469a90580a9c27aa2e90285e6ed0acf
Christian Mendez <christianmendez@Christians-MBP.attlocal.net>
2022-11-25 12:24:22 -0600

    color comparing with time works

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -35,57 +35,52 @@ saveBtnEl.on('click', function () {
 
 });
 
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
+// TODO: Add code to apply the past, present, or future class to each time
+// block by comparing the id to the current hour. HINTS: How can the id
+// attribute of each time-block be used to conditionally add or remove the
+// past, present, and future classes? How can Day.js be used to get the
+// current hour in 24-hour time?
 
 
-  // The each() method specifies a function to run for each matched element.
-  // The parseInt method parses a value as a string and returns the first integer.
-
-
-
-
-  function compTime () {                                        //COMPARING TIME FUNCTION
-    var currentDayTime = dayjs().hour();                        //CURRENT DAY & TIME
-
-    $('.time-block').each(function (){                          //JQUERY FUNCTION FOR .TIMEBLOCK CLASS, FOR "EACH" APPLYS TO ALL .TIME-BLOCK ELEMENTS
-      var timeBlockNow = parseInt($(this).attr('id'));          //VARAIBLE TIMEBLOCKNOW, USES THE PERSEINT METHOD PARSES THE ID STRING VALUE AND RETURNS IT AS INTEGER. 
-
-      if(timeBlockNow === currentDayTime) {                     //IF STATEMENT COMPARING CURRENT DAY/TIME TO THE TIME-BLOCKS TIME
-        $(this).addClass('present');                            //USING 'THIS' TO ADDCLASS AS PRESENT IF BOTH TIMES ARE EQUAL
-      }
-
-
-
-    })
-
-
-    
-  }
+// The each() method specifies a function to run for each matched element.
+// The parseInt method parses a value as a string and returns the first integer.
 
 
 
 
+function compTime() {                                         //COMPARING TIME FUNCTION
+  var currentDayTime = dayjs().hour();                        //CURRENT DAY & TIME
 
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+  $('.time-block').each(function () {                         //JQUERY FUNCTION FOR .TIMEBLOCK CLASS, FOR "EACH" APPLYS TO ALL .TIME-BLOCK ELEMENTS
+    var timeBlockNow = parseInt($(this).attr('id'));          //VARAIBLE TIMEBLOCKNOW, USES THE PERSEINT METHOD PARSES THE ID STRING VALUE AND RETURNS IT AS INTEGER. 
+
+    if (timeBlockNow < currentDayTime) {                      //IF STATEMENT COMPARING CURRENT DAY/TIME TO THE TIME-BLOCKS TIME
+      $(this).addClass('past');                               //USING 'THIS' TO ADDCLASS TO PAST, PRESENT, FUTURE
+    } else if (timeBlockNow === currentDayTime) {
+      $(this).addClass('present');
+    } else {
+      $(this).addClass('future');
+    }
+
+  })
+}
+
+// TODO: Add code to get any user input that was saved in localStorage and set
+// the values of the corresponding textarea elements. HINT: How can the id
+// attribute of each time-block be used to do this?
+//
+// TODO: Add code to display the current date in the header of the page.
 
 
 $('#9>textarea').val(localStorage.getItem('9'))
-$('#9>textarea').val(localStorage.getItem('9'))
-$('#9>textarea').val(localStorage.getItem('9'))
-$('#9>textarea').val(localStorage.getItem('9'))
-$('#9>textarea').val(localStorage.getItem('9'))
-$('#9>textarea').val(localStorage.getItem('9'))
-$('#9>textarea').val(localStorage.getItem('9'))
-$('#9>textarea').val(localStorage.getItem('9'))
-$('#9>textarea').val(localStorage.getItem('9'))
+$('#10>textarea').val(localStorage.getItem('10'))
+$('#11>textarea').val(localStorage.getItem('11'))
+$('#12>textarea').val(localStorage.getItem('12'))
+$('#13>textarea').val(localStorage.getItem('13'))
+$('#14>textarea').val(localStorage.getItem('14'))
+$('#15>textarea').val(localStorage.getItem('15'))
+$('#16>textarea').val(localStorage.getItem('16'))
+$('#17>textarea').val(localStorage.getItem('17'))
 
 compTime();
 
